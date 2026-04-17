8407e5ef78efeec1e3cbb2ecbf334395dda54dc0
Laysa-9E-ay2526 <laysa.dc@obmontessori.edu.ph>
2026-04-17 10:34:06 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function opengate (){
+    let carCounter = 18;
+    const maxCars = 50;
+
+    while (carCounter < maxCars) {
+        window.alert("Carnumber" + carCounter + " Is Entering");
+        carCounter += 1;
+    }
+
+
+
+
+}
